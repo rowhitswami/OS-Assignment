caaf66d66ded4efa07d4c9c14d4dac129958522b
Rohit Swami <rowhitswami1@gmail.com>
2018-03-29 16:51:39 +0530

    Add Problem Files

---
+++ problem_2.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 14:25:19 2018
+
+@author: Rohit Swami
+
+Question:
+Ten students (s1,s2,s3,s4,s5,s6,s7,s8,s9,s10) are going to attend an event. 
+There are lots of gift shops, they all are going to the gift shops and randomly
+ picking the gifts. After picking the gifts they are randomly arriving in the
+ billing counter. The accountant gives the preference to that student who has 
+ maximum number of gifts. Create a C program to define order of billed 
+ students?
+"""
+
+import algorithms.sorting as algo
+
+my_list = [5,6,56,12,45,3]
+print(algo.bubble_sort.sort(my_list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
