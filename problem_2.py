322ac628357ab050047e0ece200f4596e355411a
Rohit Swami <rowhitswami1@gmail.com>
2018-04-16 00:32:19 +0530

    Updated Bubble Sort with descending order

--- problem_2.py
+++ problem_2.py
@@ -17,19 +17,19 @@ There are lots of gift shops, they all are going to the gift shops and randomly
 import random
 from random import shuffle
 
-# Sorting using Bubble Sort
+# Sorting using Bubble Sort (Descending Order)
 def bubbleSort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1] :
+            if arr[j] < arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 gift = []
 # Populating list with random numbers (gifts)
 gift = (random.sample(range(1, 50), 10))
 
-# sorting items in descending order
+# Calling Bubble Sort function
 bubbleSort(gift)
 
 # Creating students
